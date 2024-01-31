4a6da649ccdf43648e04d7122402639348326bfc
Saba Giorkhelidze <sabagiorkhelidze@gmail.com>
2024-02-01 00:54:14 +0400

    tested db, with additional methods, fixed some bugs and orginized file structure as much as possible

--- backend/Classes/DBQueries.js
+++ backend/Classes/DBQueries.js
@@ -1,12 +1,13 @@
+import { json } from "express";
 import pkg from "sqlite3";
 const { Database } = pkg;
 
-class DBCommands {
+class DBQueries {
   constructor(databaseName, callback) {
     this.db = new Database(databaseName, callback);
 
-    // SQL Commands
-    this.createTablePostsSQL = `
+    // Query Commands
+    this.createTablePostsQuery = `
     CREATE TABLE IF NOT EXISTS posts (
       id INTEGER PRIMARY KEY,
       title TEXT,
@@ -15,7 +16,7 @@ class DBCommands {
     );
     `;
 
-    this.createTableImagesSQL = `
+    this.createTableImagesQuery = `
       CREATE TABLE IF NOT EXISTS images (
         id INTEGER PRIMARY KEY,
         url TEXT,
@@ -24,40 +25,63 @@ class DBCommands {
       );
     `;
 
-    this.insertPostSQL = `
-      INSERT INTO posts (title, content) VALUES (?, ?);
+    this.insertPostQuery = `
+      INSERT INTO posts (title, content, date) VALUES (?, ?, ?);
     `;
 
-    this.insertImageSQL = `
+    this.insertImageQuery = `
       INSERT INTO images (url, post_id) VALUES (?, ?);
     `;
 
-    this.selectPostsSQL = `
-      SELECT id, title, content, Date FROM posts WHERE id=(id) values (?);
+    this.selectAllPostsQuery = `
+      SELECT * FROM posts;
     `;
 
-    this.selectImagesByPostIdSQL = `
-      SELECT id, url FROM images WHERE post_id = ?;
-    `;
+    // this.selectImagesByPostIdQuery = `
+    //   SELECT id, url FROM images WHERE post_id = ?;
+    // `;
 
-    // this.deletePostSQL = `
+    // this.deletePostQuery = `
     //   DELETE FROM posts WHERE condition;
     // `;
 
     // this.createTable();
   }
 
+
+  // executable methods
   createTable() {
-    this.db.run(this.createTablePostsSQL, (err) => {
+    this.db.run(this.createTablePostsQuery, (err) => {
       if (err) console.error(err.message);
       else console.log('Table "posts" created');
     });
 
-    this.db.run(this.createTableImagesSQL, (err) => {
+    this.db.run(this.createTableImagesQuery, (err) => {
       if (err) console.error(err.message);
       else console.log('Table "images" created');
     });
   }
+
+  insertPost(title, content, date) {
+    this.db.run(this.insertPostQuery, [title, content, date], function (err) {
+      if (err) {
+        return console.error(err.message);
+      }
+      console.log(`A row has been inserted with id ${this.lastID}`);
+    });
+  }
+
+  selectAll(callback) {
+    this.db.all(this.selectAllPostsQuery, (err, rows) => {
+      if (err) {
+        console.log(err.message);
+        callback(err, null);
+      } else {
+        console.log("All rows selected");
+        callback(null, rows);
+      }
+    });
+  }
 }
 
-export default DBCommands;
+export default DBQueries;
--- backend/app.js
+++ backend/app.js
@@ -1,55 +1,27 @@
 import express from "express";
 import cors from "cors";
-// import pkg from "sqlite3";
-import DBCommands from "./Classes/DBQueries.js";
-// const { Database } = pkg;
+import DBQueries from "./Classes/DBQueries.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const db = new DBCommands("./database/Post.db", (error) => {
+const db = new DBQueries("./database/Post.db", (error) => {
   if (error) return console.log(error.message);
-  console.log("Database created");
+  //   console.log("Database created");
 });
 
-db.createTable()
-
-// Create 'posts' table
-//   db.run(
-//     `
-//     CREATE TABLE IF NOT EXISTS posts (
-//       id INTEGER PRIMARY KEY,
-//       title TEXT,
-//       content TEXT
-//     )
-//   `,
-//     (err) => {
-//       if (err) console.error(err.message);
-//       else console.log('Table "posts" created');
-//     }
-//   );
-
-//   // Create 'images' table with a foreign key referencing 'posts' table
-//   db.run(
-//     `
-//     CREATE TABLE IF NOT EXISTS images (
-//       id INTEGER PRIMARY KEY,
-//       url TEXT,
-//       post_id INTEGER,
-//       FOREIGN KEY (post_id) REFERENCES posts(id)
-//     )
-//   `,
-//     (err) => {
-//       if (err) console.error(err.message);
-//       else console.log('Table "images" created');
-//     }
-//   );
-// });
+// db.createTable();
+// db.insertPost("Post 1", 'lorem ipsum', `${Date.now()}`)
 
 app.get("/", (request, response) => {
-  return response.status(200).send("Welcome To MERN Stack Tutorial");
+  db.selectAll((err, allPost) => {
+    if (err) {
+      return response.status(500).send({ error: "Error retrieving data" });
+    }
+    return response.status(200).send({ data: allPost });
+  });
 });
 
 app.listen(8080, () => {
